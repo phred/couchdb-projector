ead555bdbc997d744e00a8beeff5e3465088fab8
lalyos <gipszlyakab@gmail.com>
2010-11-08 16:00:26 +0100

    fading/ prevText/ nextText option configuration is added

--- lists/present.js
+++ lists/present.js
@@ -4,6 +4,7 @@ function (head, req) {
 
     provides('html', function () {
         presentation = getRow().value;
+        if (presentation.options) presentation.optionsAsJSON = JSON.stringify(presentation.options);
         send(Mustache.to_html(templates.default, presentation));
     });
 }
